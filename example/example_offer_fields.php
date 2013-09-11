f4f103705c84c7b53703532ce622345cde7b561f
adc-tcarvalho <tiago.carvalho@adclick.pt>
2013-09-11 16:41:52 -0300

    same format as send_lead example

--- example/example_offer_fields.php
+++ example/example_offer_fields.php
@@ -3,10 +3,21 @@
 require_once '../lib/BeeleadsAffiliate.class.php';
 
 $blds = new BeeleadsAffiliate('your-affiliate-id', 'your-api-secret', 'beeleads-offer-id');
-/*
- * It will get the mandatory fields of an offer 
+$blds = new BeeleadsAffiliate('1184', 'bldzplataformz', '224');
+
+/**
+ * This will return all required fieldnames for the offer
  */
-$arr_response = $blds->get_offer_fields();
+$arr_response = $blds->getOfferRequiredFieldnames();
 
+if (true == $arr_response['status'])
+{
+    echo "OK - Lead integrated\n";
+}
+else
+{
+    echo "ERROR - Lead did not integrate\n";
+}
+
+echo "Response: \n";
 print_r($arr_response);
-
